a0c960af945562233dd542c56d0d0907281e0e02
Wailinn Saw <Wailinn.Saw@GMR.local>
2020-07-13 14:28:02 -0700

    Add project files.

---
+++ BinaryHeapTree/HeapTree.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Text;
+
+namespace BinaryHeapTree
+{
+    // MinHeap
+    public class HeapTree<T> where T : IComparable<T>
+    {
+        public List<T> heap;
+        public HeapTree()
+        {
+            heap = new List<T>();
+        }
+        public void Insert(T data)
+        {
+
+            //add at the end of the list.
+            heap.Add(data);
+            HeapifyUp();
+
+
+        }
+
+        public void Pop()
+        {
+
+        }
+
+        //min-heap
+        private void HeapifyUp()
+        {
+            int index = heap.Count - 1;
+            //compare temp with its parrent;
+            while (index > 0)
+            {
+                //if the current index is greater than its parrent
+                int parentIndex = (index - 1) / 2;
+
+                if (heap[index].CompareTo(heap[parentIndex]) < 0)
+                {
+                    swap(index, parentIndex);
+                }
+                index = parentIndex;
+            }
+        }
+
+        private void swap(int firstIndex, int secondIndex)
+        {
+            T temp = heap[firstIndex];
+            heap[firstIndex] = heap[secondIndex];
+            heap[secondIndex] = temp;
+        }
+    }
+}
---
+++ BinaryHeapTree/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BinaryHeapTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HeapTree<int> heap = new HeapTree<int>();
+
+            heap.Insert(1);
+            heap.Insert(10);
+            heap.Insert(13);
+            heap.Insert(3);
+            heap.Insert(9);
+            heap.Insert(2);
+            ;
+            
+        }
+    }
+}
